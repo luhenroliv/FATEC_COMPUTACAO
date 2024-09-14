ae97a90386da361a28fa7727783c144278c9fb92
Luis. <luis.henrique.18@outlook.com>
2024-09-14 12:57:51 -0300

    Add files via upload

---
+++ 01_IMOBILIARIA.py
@@ -0,0 +1,14 @@
+#1. A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
+def calcular_area(largura, comprimento):
+    return largura * comprimento
+
+def main():
+    largura = float(input("Digite a largura do terreno em metros: "))
+    comprimento = float(input("Digite o comprimento do terreno em metros: "))
+
+    area = calcular_area(largura, comprimento)
+
+    print(f"A área do terreno é {area} metros quadrados.")
+
+if __name__ == "__main__":
+    main()
